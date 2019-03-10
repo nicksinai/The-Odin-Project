649fd72a9046220ad5373b044db270ffd463c07c
Nicholas Sinai <nicksinai3@gmail.com>
2019-03-10 17:56:27 -0400

    Completed repeatString exercise

--- Javascript/javascript-exercises-master/repeatString/repeatString.js
+++ Javascript/javascript-exercises-master/repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, repeats) {
+    if (repeats < 0 ? true : false) {
+        return "ERROR";
+    }
 
+    let repeatedString = "";
+
+    for (let count = 0; count < repeats; count++) {
+        repeatedString += string;
+    }
+
+    return repeatedString;
 }
 
 module.exports = repeatString
